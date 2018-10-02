206551855aae22a194c63698a221216a53df0f06
Rob Lahoda <rob@roblahoda.com>
2018-10-02 19:41:57 -0400

    Variable declarations

---
+++ variables.js
@@ -0,0 +1,13 @@
+var myName = 'Rob Lahoda';
+
+myName = 'Robert';
+
+console.log(myName);
+
+var declaredVar;
+
+console.log(declaredVar);
+
+declaredVar = 'Now I am declared.';
+
+console.log(declaredVar);
