c59b909df0dd1ac3825ed13259c78d3014a63247
Rob Lahoda <rob@roblahoda.com>
2018-10-02 20:01:24 -0400

    More string variables

--- variables.js
+++ variables.js
@@ -11,3 +11,24 @@ console.log(declaredVar);
 declaredVar = 'Now I am declared.';
 
 console.log(declaredVar);
+
+
+const myConstant = 'I cannot change.';
+
+console.log(myConstant);
+
+// myConstant = 'Not changing';
+//
+// console.log(myConstant);
+
+let myLet = 'I can be changed';
+
+console.log(myLet);
+
+myLet = 'I just changed.';
+
+console.log(myLet);
+
+const myStr = 'I am a string';
+
+console.log(myStr);
