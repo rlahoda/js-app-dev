6d1fbf9a18fd31d42f96428aba49d6054d445535
Rob Lahoda <rob@roblahoda.com>
2018-10-02 21:00:55 -0400

    Added if-else example

---
+++ coffee_challenge.js
@@ -0,0 +1,6 @@
+let cupsConsumed = 1;
+if (cupsConsumed < 3) {
+  console.log("Yes I'll take another cup of coffee.");
+} else {
+  console.log("I think I'm OK for now.");
+}
---
+++ flowControl.js
@@ -0,0 +1,7 @@
+let x = false;
+
+if (x) {
+  console.log("the condition is true");
+} else if (x == false) {
+  console.log("the condition is not true");
+}
