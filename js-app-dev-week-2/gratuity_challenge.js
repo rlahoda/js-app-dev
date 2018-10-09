f431927928cd83497fa2e464a21facc5b5a24180
Rob Lahoda <rob@roblahoda.com>
2018-10-09 18:58:21 -0400

    added gratuity challenge

---
+++ js-app-dev-week-2/gratuity_challenge.js
@@ -0,0 +1,13 @@
+let billAmount = 100;
+
+
+function gratuity(billAmount) {
+  return billAmount * 0.2;
+}
+
+function totalWithGrat(billAmount) {
+  return billAmount + gratuity(billAmount);
+
+}
+
+console.log("Your total, with gratuity, is: " + totalWithGrat(billAmount));
