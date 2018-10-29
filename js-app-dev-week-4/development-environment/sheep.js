c743078f6b10abaedb3ec49a11ead0729d0d7f62
Rob Lahoda <rob@roblahoda.com>
2018-10-29 11:40:29 -0400

    completed sheep challenge

--- js-app-dev-week-4/development-environment/sheep.js
+++ js-app-dev-week-4/development-environment/sheep.js
@@ -12,7 +12,7 @@ var woolOwners = [
 ];
 
 
-var bags = haveYouAnyWool;
+
 
 var haveYouAnyWool = function() {
 	for (var i = 0; i < woolOwners.length; i++) {
@@ -20,18 +20,18 @@ var haveYouAnyWool = function() {
 	}
 	return (i);
 };
-
+var bags = haveYouAnyWool;
 
 function baabaaBlackSheep() {
 	console.log("BaaBaa BlackSheep have you any wool?");
 	if (bags > 0) {
-		console.log("yes sir, yes sir " + totalBags + " bags full");
+		console.log("yes sir, yes sir " + bags + " bags full");
   }
 }
 
 function oneForMy() {
 	for (var i = 0; i < 2; i++) {
-		people = Object.keys(woolOwners);
+		people = Object.keys(woolOwners[i]);
 		var person = people.toString();
 		console.log("one for my " + person);
 	}
@@ -41,7 +41,7 @@ baabaaBlackSheep();
 oneForMy();
 
 var boy = woolOwners[2];
-var littleBoy = Object.keys(littleBoy)[0];
+var littleBoy = Object.keys(woolOwners[2]);
 
-var whereHeLives = littleBoy.location;
+var whereHeLives = woolOwners[2].location;
 console.log("one for the " + littleBoy + " that lives " + whereHeLives);
